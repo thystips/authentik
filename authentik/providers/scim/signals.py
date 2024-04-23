19420ae80cdbdd16ab0719d8476528fd6cf94357
Jens Langhammer <jens@goauthentik.io>
2024-04-23 23:07:10 +0200

    add to user
    Signed-off-by: Jens Langhammer <jens@goauthentik.io>

--- authentik/providers/scim/signals.py
+++ authentik/providers/scim/signals.py
@@ -1,12 +1,13 @@
 """SCIM provider signals"""
 
 from django.db.models import Model
-from django.db.models.signals import m2m_changed, post_save, pre_delete
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 from pydanticscim.responses import PatchOp
 from structlog.stdlib import get_logger
 
 from authentik.core.models import Group, User
+from authentik.lib.models import pre_soft_delete
 from authentik.lib.utils.reflection import class_to_path
 from authentik.providers.scim.models import SCIMProvider
 from authentik.providers.scim.tasks import scim_signal_direct, scim_signal_m2m, scim_sync
@@ -29,8 +30,8 @@ def post_save_scim(sender: type[Model], instance: User | Group, created: bool, *
     scim_signal_direct.delay(class_to_path(instance.__class__), instance.pk, PatchOp.add.value)
 
 
-@receiver(pre_delete, sender=User)
-@receiver(pre_delete, sender=Group)
+@receiver(pre_soft_delete, sender=User)
+@receiver(pre_soft_delete, sender=Group)
 def pre_delete_scim(sender: type[Model], instance: User | Group, **_):
     """Pre-delete handler"""
     if not SCIMProvider.objects.filter(backchannel_application__isnull=False).exists():
